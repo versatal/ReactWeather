ea76169f567b1fb7c0720f64817b4bd517fb68ed
Roger Hardin <volcivar@yahoo.com>
2018-01-04 05:25:22 -0500

    bug fix openweather on heroku

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -8,14 +8,13 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function (res) {
-      debugger;
       if (res.data.cod && res.data.message) {
-        throw new Error(res.data.message);
+        throw new Error(err.response.data.message);
       } else {
         return res.data.main.temp
       }
     }, function (res) {
-      throw new Error(res.data.message);
+      throw new Error(err.response.message);
     });
   }
 }
