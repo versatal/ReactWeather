c635012f2d35dda782fbba51c0f640faba5bb2d0
Roger Hardin <volcivar@yahoo.com>
2018-01-05 05:55:43 -0500

    Add copy to and style About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>Here at ReactWeather you can enter any city on the globe abd get it current temperature.</p>
+      <p>We use <a href='https://openweathermap.org/'>OpeWeatherMap</a> to power this.</p>
     </div>
   )
 };
