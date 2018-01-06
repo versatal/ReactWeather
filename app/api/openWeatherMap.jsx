7d23e3e222007980e43f2fe4b3e99b61fdf1a620
Roger Hardin <volcivar@yahoo.com>
2018-01-06 07:29:47 -0500

    add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -9,11 +9,11 @@ module.exports = {
 
     return axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
-        throw new Error(err.response.data.message);
+        throw new Error(res.data.message);
       } else {
         return res.data.main.temp
       }
-    }, function (res) {
+    }, function (err) {
       throw new Error(err.response.data.message);
     });
   }
